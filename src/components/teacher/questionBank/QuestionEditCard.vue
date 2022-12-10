c6c6aecec16e22ae5d415207b3bc4f8d56824b99
安润鲁 <anrunlu@163.com>
2022-12-10 16:06:30 +0800

    opt:题目详细布局初步

--- src/components/teacher/questionBank/QuestionEditCard.vue
+++ src/components/teacher/questionBank/QuestionEditCard.vue
@@ -1,57 +1,170 @@
 <template>
   <q-card>
     <!-- 标题栏 -->
-    <CardBar title="题目编辑" icon="edit" toolbar> </CardBar>
+    <CardBar title="题目编辑" icon="edit"> </CardBar>
 
-    <q-card-section style="max-height: 80vh" class="scroll">
+    <q-card-section style="max-height: 90vh" class="scroll">
       <div class="row justify-center">
-        <div class="col-8">
+        <div class="col-8 q-gutter-md">
+          <div class="row">
+            <!-- 题目类型 -->
+            <q-select
+              class="col-4"
+              v-model="questionDetails.type"
+              :options="questionTypes"
+              square
+              outlined
+              dense
+              disable
+            >
+              <template v-slot:selected>
+                题目类型：
+                <QuestionChip
+                  :questionType="questionDetails.type"
+                  v-if="questionDetails.type"
+                />
+                <q-badge v-else>*none*</q-badge>
+              </template>
+            </q-select>
+          </div>
+
           <!-- 题干区域 -->
           <div>
-            <QuestionChip :questionType="questionDetails.type" />
-            <q-editor v-model="questionDetails.content" min-height="5rem" />
+            <q-chip
+              size="sm"
+              class="q-mx-none q-mb-sm"
+              square
+              outline
+              icon="title"
+              color="primary"
+              text-color="white"
+              label="题干"
+            />
+            <ckeditor
+              :editor="editor"
+              v-model="questionDetails.content"
+              :config="editorConfig"
+            ></ckeditor>
           </div>
+
           <!-- 客观题选项区域 -->
           <q-list v-if="questionDetails.type != '解答'">
-            <q-item
-              clickable
-              v-ripple
-              v-for="(option, index) in questionDetails.answer"
-              :key="index"
-            >
-              <q-item-section avatar v-if="questionDetails.type != '填空'">
-                <q-icon :color="option.isRight ? 'positive' : 'primary'">{{
-                  option.mark
-                }}</q-icon>
-              </q-item-section>
-              <q-item-section avatar v-else>
-                <q-icon :color="option.isRight ? 'positive' : 'primary'">{{
-                  option.mark.slice(1, 2)
-                }}</q-icon>
-              </q-item-section>
-              <q-item-section>
-                <div class="text-body2" v-html="option.content"></div>
-              </q-item-section>
-            </q-item>
+            <div>
+              <q-chip
+                clickable
+                size="sm"
+                class="q-mx-none q-mb-sm"
+                square
+                outline
+                icon="checklist_rtl"
+                color="primary"
+                text-color="white"
+                label="选项"
+                @click="optionAreaShowContent = '选项'"
+              />
+              <q-chip
+                clickable
+                size="sm"
+                class="q-mx-none q-mb-sm q-ml-sm"
+                square
+                outline
+                icon="feed"
+                color="secondary"
+                text-color="white"
+                label="解析"
+                @click="optionAreaShowContent = '解析'"
+              />
+            </div>
+
+            <div v-if="optionAreaShowContent === '选项'">
+              <q-item
+                v-for="(option, index) in questionDetails.answer"
+                :key="index"
+              >
+                <q-item-section avatar v-if="questionDetails.type != '填空'">
+                  <q-icon :color="option.isRight ? 'positive' : 'primary'">{{
+                    option.mark
+                  }}</q-icon>
+                </q-item-section>
+                <q-item-section avatar v-else>
+                  <q-icon :color="option.isRight ? 'positive' : 'primary'">{{
+                    option.mark.slice(1, 2)
+                  }}</q-icon>
+                </q-item-section>
+                <q-item-section>
+                  <q-editor
+                    v-model="option.content"
+                    min-height="2rem"
+                    square
+                    v-if="questionDetails.type != '判断'"
+                  />
+                  <div class="text-body2" v-html="option.content" v-else></div>
+                </q-item-section>
+                <!-- 右侧按钮 -->
+                <q-item-section side v-if="questionDetails.type != '判断'">
+                  <q-btn
+                    size="sm"
+                    dense
+                    round
+                    outline
+                    color="red-4"
+                    icon="remove"
+                    @click.stop=""
+                  />
+                </q-item-section>
+              </q-item>
+              <q-item v-if="questionDetails.type != '判断'">
+                <q-item-section>
+                  <q-btn
+                    style="width: 30%; margin: 0 auto"
+                    dense
+                    flat
+                    color="green-4"
+                    icon="add"
+                    label="添加选项"
+                  />
+                </q-item-section>
+              </q-item>
+            </div>
+
+            <!-- 题目解析 -->
+            <div v-else-if="optionAreaShowContent === '解析'">
+              <ckeditor
+                :editor="editor"
+                v-model="questionDetails.explain"
+                :config="editorConfig"
+              ></ckeditor>
+            </div>
           </q-list>
+
           <!-- 解答题答案区域 -->
           <div v-else>
-            <div
-              class="text-body2 q-pt-sm"
-              v-html="questionDetails.answer.content"
-            ></div>
+            <q-chip
+              size="sm"
+              class="q-mx-none q-mb-sm"
+              square
+              outline
+              icon="fact_check"
+              color="primary"
+              text-color="white"
+              label="答案"
+            />
+            <ckeditor
+              :editor="editor"
+              v-model="questionDetails.answer.content"
+              :config="editorConfig"
+            ></ckeditor>
           </div>
         </div>
       </div>
     </q-card-section>
-
-    <q-card-actions align="center">
-      <q-btn class="q-ml-sm" color="primary" icon="edit" label="编辑" />
-    </q-card-actions>
   </q-card>
 </template>
 
 <script>
+import Editor from "ckeditor5-custom-build/build/ckeditor";
+import { MyClipboardAdapterPlugin } from "src/utils/ckeditor/MyClipboardPlugin";
+import { MyCustomUploadAdapterPlugin } from "src/utils/ckeditor/MyUploadPlugin";
 import { marked } from "marked";
 import { apiGetQuestionDetail } from "src/api/teacher/questionBank";
 import QuestionChip from "src/components/common/QuestionChip.vue";
@@ -61,9 +174,23 @@ export default {
   props: ["questionId"],
   data() {
     return {
+      // 编辑器配置
+      editor: Editor,
+      editorConfig: {
+        extraPlugins: [MyClipboardAdapterPlugin, MyCustomUploadAdapterPlugin],
+      },
+
+      // 题目详细信息
       questionDetails: {
+        type: "",
         content: "",
       },
+
+      // 题目类型
+      questionTypes: ["单选", "多选", "判断", "填空", "解答"],
+
+      // 题目下方显示内容
+      optionAreaShowContent: "选项",
     };
   },
 
