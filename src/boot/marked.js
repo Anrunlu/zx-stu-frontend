9c328ccbaf05e1bf7f4c1ae906fa3e213dcbe154
安润鲁 <anrunlu@163.com>
2022-12-12 15:44:18 +0800

    opt:题目详情渲染katex

--- src/boot/marked.js
+++ src/boot/marked.js
@@ -1,8 +1,13 @@
 import { marked } from "marked";
 import markedKatex from "marked-katex-extension";
+import Katex from "vue-katex-auto-render";
 
 const options = {
   throwOnError: false,
 };
 
 marked.use(markedKatex(options));
+
+export default async ({ Vue }) => {
+  Vue.directive("katex", Katex);
+};
