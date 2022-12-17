ea46c7fdf26d8eebb9074461cf5d444f6595558b
安润鲁 <anrunlu@163.com>
2022-12-17 08:59:24 +0800

    todo

--- src/pages/teacher/QuestionAdd.vue
+++ src/pages/teacher/QuestionAdd.vue
@@ -263,7 +263,7 @@
         </div>
       </div>
       <div class="float-right q-mr-sm">
-        <q-btn flat square label="预览" icon="visibility" />
+        <!-- <q-btn flat square label="预览" icon="visibility" /> -->
         <q-btn
           flat
           square
