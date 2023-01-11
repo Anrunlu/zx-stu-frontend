055f5b6e513617ffa5af539dcd4739274d8767e6
安润鲁 <anrunlu@163.com>
2023-01-11 11:51:02 +0800

    opt:登录页面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,11 @@
 <template>
   <q-layout>
     <q-page-container>
-      <q-page class="flex flex-center bg-normal-gradient row">
+      <q-page class="bg-grey-1 q-pt-xl">
         <div class="absolute-top-right">
           <q-btn
-            color="white"
+            class="q-pa-sm"
+            color="primary"
             flat
             round
             icon="feedback"
@@ -12,126 +13,98 @@
           >
             <q-tooltip>反馈通道</q-tooltip></q-btn
           >
-          <q-btn
-            color="white"
-            flat
-            round
-            @click="$q.dark.toggle()"
-            :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"
-          />
         </div>
-        <!-- 登录框 -->
-        <q-card class="login-form-card col-11 col-sm-8 col-md-6">
-          <q-card-section>
-            <div class="row">
-              <!-- 左侧，小屏幕隐藏 -->
-              <div class="col-6" v-show="$q.screen.gt.sm">
-                <div class="column full-height">
+
+        <div class="row justify-center q-pt-xl">
+          <!-- logo -->
+          <div class="col-11 text-center">
+            <q-img
+              :width="$q.screen.gt.sm ? '300px' : '250px'"
+              src="~assets/teacherLogo.png"
+              spinner-color="primary"
+              spinner-size="82px"
+            />
+          </div>
+
+          <!-- 登录框 -->
+          <div class="col-10 col-sm-6 col-md-4 col-xl-3 q-pt-xl">
+            <q-card>
+              <q-card-section class="row justify-between items-end">
+                <div class="text-h5 col">登录</div>
+                <div class="col desktop-only">
+                  <a class="float-right">扫码登录</a>
+                </div>
+              </q-card-section>
+              <q-card-section>
+                <!-- 用户名 -->
+                <q-input
+                  class="logon-input"
+                  clearable
+                  bottom-slots
+                  label="账号"
+                  v-model="userLoginDto.username"
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="account_circle" />
+                  </template>
+                </q-input>
+                <!-- 密码 -->
+                <q-input
+                  class="logon-input"
+                  bottom-slots
+                  label="密码"
+                  :type="isShowPwd ? 'password' : 'text'"
+                  v-model="userLoginDto.password"
+                  @keyup.enter.native="handleLogin"
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="vpn_key" />
+                  </template>
+                  <template v-slot:append>
+                    <q-icon
+                      :name="isShowPwd ? 'visibility_off' : 'visibility'"
+                      class="cursor-pointer"
+                      @click="isShowPwd = !isShowPwd"
+                    />
+                  </template>
+                </q-input>
+                <div class="row justify-between items-center">
+                  <q-checkbox left-label label="记住我" v-model="isRemeberMe" />
+
                   <div class="col">
-                    <div class="row items-center">
-                      <q-avatar size="60px">
-                        <img
-                          src="https://cyberdownload.anrunlu.net/zxV2_logo.png"
-                        />
-                      </q-avatar>
-                      <!-- 平台名称 -->
-                      <div class="text-h5 text-center q-ml-md">
-                        知新教学过程管理平台
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-6 q-ma-md">
-                    <!-- 平台简介 -->
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Adipisci dolores cupiditate eveniet est odio impedit
-                      facere beatae nobis atque autem unde laborum quibusdam
-                      asperiores rem, ipsa ut numquam id sequi!
-                    </p>
-                  </div>
-                  <!-- 底部 -->
-                  <div class="col-2">
-                    <div class="row">
-                      <!-- 版权信息等 -->
-                      <span class="col text-center" style="padding: 20px 0">
-                        网络空间安全学院数字大脑平台
-                      </span>
-                    </div>
+                    <a class="float-right">忘记密码</a>
                   </div>
                 </div>
-              </div>
-              <q-separator vertical inset v-if="$q.screen.gt.sm" />
-              <!-- 右侧 -->
-              <div class="col flex justify-center items-center">
-                <q-card
-                  square
-                  style="width: 90%; height: 100%"
-                  class="no-shadow"
-                >
-                  <q-card-section class="row justify-between items-end">
-                    <div class="text-h5 col">登录</div>
-                    <div class="col">
-                      <a class="float-right">扫码登录</a>
-                    </div>
-                  </q-card-section>
-                  <q-card-section>
-                    <!-- 用户名 -->
-                    <q-input
-                      class="logon-input"
-                      clearable
-                      bottom-slots
-                      label="账号"
-                      v-model="userLoginDto.username"
-                    >
-                      <template v-slot:prepend>
-                        <q-icon name="account_circle" />
-                      </template>
-                    </q-input>
-                    <!-- 密码 -->
-                    <q-input
-                      class="logon-input"
-                      bottom-slots
-                      label="密码"
-                      :type="isShowPwd ? 'password' : 'text'"
-                      v-model="userLoginDto.password"
-                      @keyup.enter.native="handleLogin"
-                    >
-                      <template v-slot:prepend>
-                        <q-icon name="vpn_key" />
-                      </template>
-                      <template v-slot:append>
-                        <q-icon
-                          :name="isShowPwd ? 'visibility_off' : 'visibility'"
-                          class="cursor-pointer"
-                          @click="isShowPwd = !isShowPwd"
-                        />
-                      </template>
-                    </q-input>
-                    <div class="row justify-between items-center">
-                      <q-checkbox
-                        left-label
-                        label="记住我"
-                        v-model="isRemeberMe"
-                      />
+                <!-- 登录按钮 -->
+                <q-btn
+                  class="full-width q-mt-sm"
+                  color="primary"
+                  icon-right="send"
+                  label="登录"
+                  @click="handleLogin"
+                />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
 
-                      <div class="col">
-                        <a class="float-right">忘记密码</a>
-                      </div>
-                    </div>
-                    <!-- 登录按钮 -->
-                    <q-btn
-                      class="full-width q-mt-sm"
-                      color="primary"
-                      icon-right="send"
-                      label="登录系统"
-                      @click="handleLogin"
-                    />
-                  </q-card-section>
-                </q-card>
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+        <!-- 底部信息 -->
+        <div
+          id="login-footer"
+          class="fixed-bottom text-center text-grey q-pb-lg"
+        >
+          <div v-if="$q.screen.gt.sm">
+            <a href="https://c605.cn">关于我们</a>
+            <span class="q-px-sm"> | </span>
+            <a>网络空间安全学院数字大脑平台 All Rights Reserved. </a>
+          </div>
+
+          <div v-else>
+            <a>网络空间安全学院数字大脑平台 All Rights Reserved. </a>
+            <br />
+            <a href="https://c605.cn">关于我们</a>
+          </div>
+        </div>
       </q-page>
     </q-page-container>
   </q-layout>
@@ -194,5 +167,14 @@ export default {
 }
 .login-form-card a {
   cursor: pointer;
+}
+#login-footer a {
+  text-decoration: none;
+  color: grey;
+}
+#login-footer a:hover {
+  cursor: pointer;
+  color: grey;
+  text-decoration: underline;
 }
 </style>
