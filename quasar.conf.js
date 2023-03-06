97b8c551674b9fb2b8009be873d736b4de3bb093
安润鲁 <anrunlu@163.com>
2023-03-06 18:04:04 +0800

    opt

--- quasar.conf.js
+++ quasar.conf.js
@@ -58,7 +58,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: "history", // available values: 'hash', 'history'
 
       env: {
         API_URL: ctx.dev
