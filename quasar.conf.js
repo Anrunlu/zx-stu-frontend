44503752538e38bb844f4a19a6447a67133318c0
安润鲁 <anrunlu@163.com>
2023-03-07 20:02:35 +0800

    opt:固定端口号

--- quasar.conf.js
+++ quasar.conf.js
@@ -103,7 +103,7 @@ module.exports = function (ctx) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 9092,
       open: true, // opens browser window automatically
       // host: "192.168.31.51",
     },
