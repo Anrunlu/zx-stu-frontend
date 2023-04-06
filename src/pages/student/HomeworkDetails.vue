7dd6202f4a110789c3c6b67a8b1222cbb21829db
安润鲁 <anrunlu@163.com>
2023-04-06 10:39:18 +0800

    opt:优化实时通知组件

--- src/pages/student/HomeworkDetails.vue
+++ src/pages/student/HomeworkDetails.vue
@@ -159,6 +159,9 @@
         @prevQuestion="handlePrevQuestion"
       />
     </q-footer>
+
+    <!-- 实时通知组件 -->
+    <ImmediatelyAncmt />
   </q-layout>
 </template>
 
@@ -222,6 +225,8 @@ export default {
       import("src/components/student/studentHomework/HomeworkToolbar.vue"),
     AnswerSheetCard: () =>
       import("src/components/student/studentExam/AnswerSheetCard.vue"),
+    ImmediatelyAncmt: () =>
+      import("src/components/common/ImmediatelyAncmt.vue"),
   },
 
   computed: {
