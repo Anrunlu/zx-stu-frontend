4acb0712092aad6f636626100d81cd8d0aa97481
安润鲁 <anrunlu@163.com>
2023-05-08 17:48:01 +0800

    opt:学习资源接口

--- src/api/student/teaResource.js
+++ src/api/student/teaResource.js
@@ -4,7 +4,7 @@ import { loadingFn } from "src/utils/loadingTools";
 // 获取教学资源列表
 export function apiGetTeaResources(data) {
   return loadingFn(request)({
-    url: "/tea/tearesource/filter",
+    url: "/stu/resource/filter",
     method: "POST",
     data,
   });
